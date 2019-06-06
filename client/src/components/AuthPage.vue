e3398d275bdb64d9877934a4458328b8eca35154
Seughton <seughton@gmail.com>
2019-06-06 11:20:56 +0300

    Initial commit VUE AUTH

---
+++ client/src/components/AuthPage.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <button class="signUpButton">
+        Sign Up
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AuthPage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.signUpButton {
+  height: 40px;
+  width: 100px;
+  background: white;
+  border: 1px solid rgb(98, 98, 223);
+  cursor: pointer;
+}
+.signUpButton:hover {
+   background-color: rgba(230, 233, 185,.5);
+ }
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <AuthPage msg="Welcome to Vue Auth App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import AuthPage from '@/components/AuthPage.vue';
+
+export default {
+  name: 'home',
+  components: {
+    AuthPage,
+  },
+};
+</script>
